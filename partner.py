6f8675427836e17c749ecf8b4eb17037069864d5
Marcelo Costa <marcelo@comdesk.com.br>
2015-07-02 13:08:09 -0300

    Criado arquivo partner.py

--- partner.py
+++ partner.py
@@ -19,7 +19,16 @@
 #                                                                              #
 ################################################################################
 
-from . import controllers
-from . import models
-from . import partner
-from . import wizard
+
+# -*- coding: utf-8 -*-
+from openerp import fields, models
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    # Add a new column to the res.partner model, by default partners are not
+    # instructors
+    instructor = fields.Boolean("Instructor", default=False)
+
+    session_ids = fields.Many2many('openacademy.session',
+        string="Attended Sessions", readonly=True)
