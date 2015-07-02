ac5334a0e8d4702a9438c38281d079dee6ff4aa2
Marcelo Costa <marcelo@comdesk.com.br>
2015-07-02 12:50:13 -0300

    Alterado booleano 'true para True'

--- models.py
+++ models.py
@@ -26,7 +26,7 @@ from openerp import models, fields, api, exceptions, _
 class Course(models.Model):
     _name = 'openacademy.course'
 
-    name = fields.Char(string="Title", required=true)
+    name = fields.Char(string="Title", required=True)
     description = fields.Text()
 
     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
